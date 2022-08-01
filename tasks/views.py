40b012446ab4288c2ee2e58cd9b4225f4c3e438b
Xu Che <xuche1998@gmail.com>
2022-08-01 17:29:13 +0800

    add tasks page

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,46 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
 def index(request):
-    return HttpResponse("TASK WORKS")
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+# Add a new task:
+def add(request):
+    # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = NewTaskForm(request.POST)
+
+        # Check if form data is valid (server-side)
+        if form.is_valid():
+
+            # Isolate the task from the 'cleaned' version of form data
+            task = form.cleaned_data["task"]
+
+            # Add the new task to our list of tasks
+            tasks.append(task)
+
+            # Redirect user to list of tasks
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+
+            # If the form is invalid, re-render the page with existing information.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form" : NewTaskForm()
+    })
